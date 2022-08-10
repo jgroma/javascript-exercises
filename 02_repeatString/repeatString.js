885024371e65762655b6938d59b274df59fa0762
aspen <jgromadzinska9@gmail.com>
2022-08-10 21:18:47 +0100

    Solve exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+let string;
+let num;
+let loopString;
 
+const repeatString = function(string, num) {
+    loopString = string
+    if (num < 0) {
+        return 'ERROR';
+    } else if (num === 0) {
+        return '';
+    } else {
+     for (let i = 1; i < num; i++) {
+         loopString += string;
+     }
+    return loopString;
+    }
 };
 
 // Do not edit below this line
