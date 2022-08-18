305d7f2901f8ff67c3ce5288371aef1f8bc7b050
aspen <jgromadzinska9@gmail.com>
2022-08-18 18:35:32 +0100

    Solve exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
 
+const sumAll = function(firstNum, secondNum) {
+   let total = firstNum + secondNum;
+
+   if (typeof firstNum !== 'number' || typeof secondNum !== 'number') {
+    return 'ERROR';
+ } else if (firstNum < 0 || secondNum < 0) {
+    return 'ERROR';
+ } else if (firstNum > secondNum) {
+      for (let i = firstNum - 1; i > secondNum; i--) {
+        total += i;
+    }
+       return total;
+   } else {
+      for (let i = firstNum + 1; i < secondNum; i++) {
+          total += i;      
+   }
+      return total;
+   }
 };
 
 // Do not edit below this line
