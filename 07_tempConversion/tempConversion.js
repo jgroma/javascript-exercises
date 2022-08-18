284dbf4950c1a071e2d10d117326754e7574b63c
aspen <jgromadzinska9@gmail.com>
2022-08-18 19:23:49 +0100

    Solve exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+   const Conversion = (temp - 32) * 5/9;
+   const roundedConversion = Math.round(Conversion * 10) / 10;
+   return roundedConversion;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const Conversion = (temp * 9/5) + 32;
+  const roundedConversion = Math.round(Conversion * 10) / 10;
+  return roundedConversion;
 };
 
 // Do not edit below this line
