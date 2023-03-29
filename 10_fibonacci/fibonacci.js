545e94251ef5b65b67444ff309c497163f0d3335
aspen <jgromadzinska9@gmail.com>
2023-03-29 20:04:27 +0100

    Solve exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+    let firstNumber = 1;
+    let secondNumber = 1;
+    let total = secondNumber;
+
+    if (count < 0) {
+        return "OOPS";
+    } else if (count === 1) {
+        return 0;
+    } else if (count === 2) {
+        return 1;
+    } else {
+        for (let i = 2; i < count; i++) {
+            total = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = total;
+        }
+      return total;  
+    }
 
 };
 
