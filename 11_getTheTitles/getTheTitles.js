034bc997c334d91eb01e54a63283fad22fe360b3
aspen <jgromadzinska9@gmail.com>
2023-03-29 20:19:48 +0100

    Solve exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+  const newArray = books.map(book => `${book.title}`);
+  return newArray;
+}
 
-};
+//console.log(gethTheTitles)
 
+//const getTheTitles = books.map(book => `${book.title}`);
 // Do not edit below this line
 module.exports = getTheTitles;
