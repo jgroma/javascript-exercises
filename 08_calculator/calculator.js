645839d9f3391d8aa7da9362c898a256dd2dcb29
aspen <jgromadzinska9@gmail.com>
2023-03-30 16:35:30 +0100

    Solve exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+
+  if (array.length === 0) {
+  return 0;
+  } else {
+    let result = array.reduce((a,b) => a+b);
+    return result;
+  }
+
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = numbers.reduce((a,b) => a*b);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+   return 1;
+  } else {
+    total = a;
+  for (let i = 1; i < a; i++) {
+    total *= i;
+  }
+  return total;
+  };
 };
 
 // Do not edit below this line
